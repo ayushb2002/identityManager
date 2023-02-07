10bc4ac240f84c9435a5b6d1c4645812e4cb4162
Ayush Bansal <ayushb6732@gmail.com>
2023-02-07 20:57:13 +0530

    Updated API

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,10 +1,36 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
- 
+import abi from './Identity.json';
+var ethers = require('ethers');
+var express = require('express');
+var cors = require('cors');
+var app = express();
+require('dotenv').config();
+
+const contractAddress = process.env.CONTRACT_ADDRESS
+
+export const executeFunction = () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi.abi, signer);
+    return contract;
+};
+
+
 app.use(cors())
  
 app.listen(5000, function () {
   console.log('CORS-enabled web server listening on port 5000');
 });
 
+app.get('/', cors(), function (req, res, next) {
+  res.json({msg: 'API home page!'});
+})
+
+app.get('/lastHumanVerified/:adhaar', cors(), async function (req, res, next) {
+  const lastVerified = await contract.lastHumanVerified(req.params.adhaar);
+  res.json({lastVerified: lastVerified});
+})
+
+app.get('/idExpiry/:adhaar', cors(), async function (req, res, next) {
+  const idExp = await contract.returnIdExpiry(req.params.adhaar);
+  res.json({expiry: idExp});
+})
