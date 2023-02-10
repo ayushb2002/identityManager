6c7c3bf6c2058355f0517c1979c7018854abc2ab
Ganesh G Setty <ganeshsetty.04@gmail.com>
2023-02-10 21:24:29 +0530

    aadhar functions done

--- backend/python_api/api.py
+++ backend/python_api/api.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from idcard import * 
+import keras_ocr
+
+pipeline = keras_ocr.pipeline.Pipeline(scale=2)
 
 app = Flask(__name__)
-config = get_config()
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 
-@app.route("/aadhar_verify",methods=['POST'])
-def aadhar_verify(request):
+@app.route("/aadhar_verify")
+def aadhar_verify():
     print("Started")
+    aadhar_check(pipeline,'aadhar2.jpg')
+    return "done"
     # imagefile =request.files.get('file')
     # imagefile.save(imagefile.filename)
     # imagefilepath=imagefile.filename
@@ -29,3 +34,4 @@ def aadhar_verify(request):
 # Adhaar, Wallet -> If this identity exists and belongs to the user (true) otherwise false
 # If true, we send otp to the owner's mail address
 # Website POST OTP -> Otp verified -> Logged in (true) else false
+app.run(debug=True)
--- backend/python_api/idcard.py
+++ backend/python_api/idcard.py
@@ -1,16 +1,58 @@
-import keras_ocr
-pipeline = keras_ocr.pipeline.Pipeline(scale=1)
-images = [
-    keras_ocr.tools.read(url) for url in [
-        'aadhar.jpg',
-        'id.jpg',
+def gender(data):
+    for x in data:
+        if x[0].lower()=="male":
+            return 'male'
+    return 'female'
+
+def name_check(name,data):
+    name=sorted(list(name.split()))
+    datatemp=[x[0] for x in data]
+    for i in range(len(datatemp)-len(name)):
+        # print(datatemp[i:i+len(name)])
+        if sorted(datatemp[i:i+len(name)])==name:
+            return True
+    return False
+
+def phone_number(data):
+    possible=[]
+    for x in data:
+        if len(x[0])==10:
+            try:
+                int(x[0])
+                possible.append(x[0])
+            except:
+                pass
+    return possible[-1]
+
+def get_aadhar(data):
+    aadhar=[]
+    for x in data:
+        if len(x[0])==4:
+            try:
+                int(x[0])
+                aadhar.append(x)
+            except:
+                pass
+    aadhar.sort(key=lambda x: (x[1][0][1]),reverse=True)
+    # print(aadhar)
+    return ' '.join([x[0] for x in aadhar])
+
+def aadhar_check(pipeline,path):
+    import keras_ocr
+    images = [
+        keras_ocr.tools.read(url) for url in [
+            path,
+            
+        ]
     ]
-]
-print(len(images))
-from pprint import pprint
-prediction_groups = pipeline.recognize(images)
-pprint(prediction_groups)
-sorted_aadhaar = sorted(prediction_groups[0], key=lambda x: (x[1][0][1],x[1][0][0]))
-[print(x[0]) for x in sorted_aadhaar]
-sorted_id = sorted(prediction_groups[1], key=lambda x: (x[1][0][1],x[1][0][0]))
-[print(x[0]) for x in sorted_id]
+    print(len(images))
+    from pprint import pprint
+    prediction_groups = pipeline.recognize(images)
+
+    sorted_aadhaar = sorted(prediction_groups[0], key=lambda x: (x[1][0][1],x[1][0][0]))
+
+    print(gender(sorted_aadhaar),name_check('ganesh gummalam setty',sorted_aadhaar),phone_number(sorted_aadhaar),get_aadhar(sorted_aadhaar))
+    # [print(x[0]) for x in sorted_aadhaar]
+    # sorted_id = sorted(prediction_groups[1], key=lambda x: (x[1][0][1],x[1][0][0]))
+    # [print(x[0]) for x in sorted_id]
+
