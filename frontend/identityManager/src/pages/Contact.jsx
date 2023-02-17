8e451a4b0d834b99be86148faf4e503cab19789a
Ayush Bansal <ayushb6732@gmail.com>
2023-02-17 23:19:46 +0530

    Built home and contact page

--- frontend/identityManager/src/pages/Contact.jsx
+++ frontend/identityManager/src/pages/Contact.jsx
@@ -1,17 +1,99 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Contact = () => {
   return (
     <section>
-        <Navbar />
-        <div className='min-h-screen'>
+      <Navbar />
+      <div className="p-5 flex justify-center">
+        <span className="text-3xl">Meet the team</span>
+      </div>
+      <div className="p-5 grid grid-cols-3">
+        {/* Identity card begins */}
 
+        <div className="card w-96 bg-base-100 shadow-xl my-10">
+          <figure>
+            <img
+              src="https://picsum.photos/400/200?random=1"
+              alt="human"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Ayush Bansal</h2>
+            <p>ML Developer, Blockchain developer, Web Developer</p>
+            <div className="card-actions justify-end">
+              <a href="#"><button className="btn btn-primary">Contact me</button></a>
+            </div>
+          </div>
         </div>
-        <Footer />
-    </section>
-  )
-}
 
-export default Contact
+        {/* Identity card ends */}
+
+        {/* Identity card begins */}
+
+        <div className="card w-96 bg-base-100 shadow-xl my-10">
+          <figure>
+            <img
+              src="https://picsum.photos/400/200?random=2"
+              alt="human"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Ganesh G. Shetty</h2>
+            <p>Web Developer</p>
+            <div className="card-actions justify-end">
+              <a href="#"><button className="btn btn-primary">Contact me</button></a>
+            </div>
+          </div>
+        </div>
+
+        {/* Identity card ends */}
+
+        {/* Identity card begins */}
+
+        <div className="card w-96 bg-base-100 shadow-xl my-10">
+          <figure>
+            <img
+              src="https://picsum.photos/400/200?random=3"
+              alt="human"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Yathansh Tewatia</h2>
+            <p>Web Developer</p>
+            <div className="card-actions justify-end">
+              <a href="#"><button className="btn btn-primary">Contact me</button></a>
+            </div>
+          </div>
+        </div>
+
+        {/* Identity card ends */}
+
+        {/* Identity card begins */}
+
+        <div className="card w-96 bg-base-100 shadow-xl my-10">
+          <figure>
+            <img
+              src="https://picsum.photos/400/200?random=4"
+              alt="human"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Vedant Agnihotri</h2>
+            <p>Web Developer</p>
+            <div className="card-actions justify-end">
+              <a href="#"><button className="btn btn-primary">Contact me</button></a>
+            </div>
+          </div>
+        </div>
+
+        {/* Identity card ends */}
+
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default Contact;
--- frontend/identityManager/src/pages/Home.jsx
+++ frontend/identityManager/src/pages/Home.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import heroImg from "../img/hero-img.png";
 
 const Home = () => {
   return (
     <section>
       <Navbar />
-      <div className='min-h-screen'>
-
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={heroImg}
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Decent ID</h1>
+            <p className="py-6">
+            The cent-erpiece of decentralized identification.
+            </p>
+            <a href="/register"><button className="btn btn-primary">Get Started</button></a>
+          </div>
+        </div>
       </div>
       <Footer />
     </section>
-  )
-}
+  );
+};
 
-export default Home 
+export default Home;
